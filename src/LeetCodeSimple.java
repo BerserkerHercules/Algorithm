c8d992ddd3e93b9c1b721cab33ec279e44de7697
Berserker <dddz97@163.com>
2019-07-29 16:51:35 +0800

    回文数

--- src/LeetCodeSimple.java
+++ src/LeetCodeSimple.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * LeetCode 简单题
  *
@@ -74,14 +77,68 @@ public class LeetCodeSimple {
         return (int)res;
     }
 
+    /**
+     * 9、回文数
+     *
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     *
+     * 示例 1:
+     *
+     * 输入: 121
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入: -121
+     * 输出: false
+     * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     * 示例 3:
+     *
+     * 输入: 10
+     * 输出: false
+     * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+     * 进阶:
+     *
+     * 你能不将整数转为字符串来解决这个问题吗？
+     *
+     * 链接：https://leetcode-cn.com/problems/palindrome-number
+     *
+     * 我的结果：
+     * 执行用时: 12 ms
+     * 在所有 Java 提交中击败了 98.63% 的用户
+     */
+    public boolean isPalindrome(int x) {
+        int y=x;
+        if(x<0||(x%10==0&&x!=0)){
+            return false;
+        }
+        int res=0;
+        do{
+            res = res*10+x%10;
+            x/=10;
+        }while(x!=0);
+        return res==y;
+    }
+
+    /**
+     * 用字符串
+     * 执行时间远大于不用字符串的方法(50ms以上)
+     */
+    public boolean isPalindrome2(int x) {
+        if(x<0||(x%10==0&&x!=0)){
+            return false;
+        }
+        String str = x+"";
+        return str.equals(new StringBuffer(str).reverse().toString());
+    }
+
 
     public static void main(String[] args){
-        LeetCodeSimple leetCode = new LeetCodeSimple();
+        LeetCodeSimple leetCodeSimple = new LeetCodeSimple();
         long startTime=System.currentTimeMillis();
-        System.out.println(leetCode.reverse(1534236469));
+        System.out.println(leetCodeSimple.isPalindrome(1234321));
         long endTime=System.currentTimeMillis();
-        System.out.println(startTime);
-        System.out.println(endTime);
+        System.out.println(endTime-startTime);
+        System.exit(0);
     }
 
 }
