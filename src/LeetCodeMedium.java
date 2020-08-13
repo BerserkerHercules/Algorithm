64e31727b3aa3ee2b36969082f241c513e2b82ab
berserkerhelculs <dddz97@163.com>
2020-08-13 14:32:37 +0800

    中等1

--- src/LeetCodeMedium.java
+++ src/LeetCodeMedium.java
@@ -10,21 +10,26 @@ public class LeetCodeMedium {
      * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
      * 7 -> 0 -> 8
      */
-//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//        ListNode res = new ListNode(0);
-//        int carry = 0;
-//        while (l1 != null || l2 != null) {
-//            int sum = l1.val + l2.val + carry;
-//            carry = sum / 10;
-//            res.next = new ListNode(sum % 10);
-//            res = res.next;
-//            l1 = l1.next;
-//            l2 = l2.next;
-//        }
-//        if(carry!=0){
-//            res.next = new ListNode(carry);
-//        }
-//        return res;
-//    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode start = new ListNode(0);
+        ListNode res = start;
+        int carry = 0;
+        int i = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            res.next = new ListNode(sum % 10);
+            res = res.next;
+            if (l1 != null) { l1 = l1.next; }
+            if (l2 != null) { l2 = l2.next; }
+            i++;
+        }
+        if (carry != 0) {
+            res.next = new ListNode(carry);
+        }
+        return start.next;
+    }
 
 }
